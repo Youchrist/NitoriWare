a05162aa17fa4f5efccd04dd7c6dd5e11a296406
Youchrist <Legacyofyouchrist@gmail.com>
2017-11-15 20:51:08 -0600

    bullet now moves!

---
+++ Assets/Resources/Microgames/ReimuDodge/Scripts/ReimuDodgebullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgebullet : MonoBehaviour
+{ void Start()
+    {
+        Invoke("SetTrajectory", delay);
+        GameObject player = GameObject.Find("Player");
+        trajectory = (player.transform.position - transform.position).normalized;
+    }
+    void SetTrajectory()
+    {
+        GameObject player = GameObject.Find("Player");
+        trajectory = (player.transform.position - transform.position).normalized;
+    }
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+   
+    private float delay = 1f;
+    private Vector2 trajectory;
+   
+    void Update()
+    {
+        if (trajectory != null)
+        {
+            Vector2 newPosition = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+    }
+}
